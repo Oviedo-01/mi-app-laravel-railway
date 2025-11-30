b5f87cc7f49711723278885556af01f3727f3e18
Oviedo-01 <estradaoviedol@gmail.com>
2025-11-30 12:34:03 -0500

    Actualizacion

--- config/app.php
+++ config/app.php
@@ -97,7 +97,7 @@ return [
 
     'cipher' => 'AES-256-CBC',
 
-    'key' => env('APP_KEY', 'base64:lfw8uySb/px81DM775XZ37XQj20pIrc9+zyw4Rlu7kg='),
+    'key' => env('APP_KEY'),
 
     'previous_keys' => [
         ...array_filter(
--- start.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "APP_KEY=${APP_KEY}" >> .env
-php artisan config:clear
-php artisan config:cache
-php artisan migrate --force
-php artisan db:seed --class=AdminUserSeeder --force
-php artisan serve --host=0.0.0.0 --port=$PORT
